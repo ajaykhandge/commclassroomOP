2e62ef5d57989904c3088148db3dfa161574328c
Ajay Khandge <ajaykhandge88@gmail.com>
2021-09-29 14:58:55 +0530

    created function for sum

--- Sum.java
+++ Sum.java
@@ -1,8 +1,12 @@
 class Sum{
 
     public static void main(String []args){
-        int sum = 1+2;
-        System.out.println("The sum is"+ sum);
+        
+        System.out.println("The sum is"+ sum(1,2));
 
+    }
+
+    public static int sum(int a,int b){
+        return a+b;
     }
 }
